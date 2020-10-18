c87aee227e0357136fc1e459b9b67a927a92cd75
laipz8200 <laipz8200@outlook.com>
2020-10-18 23:29:15 +0800

    修复咪咕点歌没有结果时触发KeyError的问题

--- search_migu_music.py
+++ search_migu_music.py
@@ -5,7 +5,6 @@ USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/
 
 async def search(keyword, result_num: int = 3):
     """ 搜索音乐 """
-    number = 5
     song_list = []
     # ?rows=20&type=2&keyword=稻香&pgc=1
 
@@ -21,20 +20,24 @@ async def search(keyword, result_num: int = 3):
         )
         res_data = await resp.json()
     except Exception as e:
-        logger.error(f'Request Migu Music Timeout {e}')
+        logger.warning(f'Request Migu Music Timeout {e}')
         return None
-    for item in res_data['musics'][:result_num]:
-        song_list.append(
-            {
-                'url': 'https://music.migu.cn/v3/music/song/' + item['copyrightId'],
-                'purl': item['mp3'],
-                'image': item['cover'],
-                'title': item['songName'],
-                'content': item['singerName'],
-                'name': item['songName'],
-                'artists': item['singerName'],
-                'type': 'custom',
-                'subtype': 'migu'
-            }
-        )
-    return song_list
+    try:
+        for item in res_data['musics'][:result_num]:
+            song_list.append(
+                {
+                    'url': 'https://music.migu.cn/v3/music/song/' + item['copyrightId'],
+                    'purl': item['mp3'],
+                    'image': item['cover'],
+                    'title': item['songName'],
+                    'content': item['singerName'],
+                    'name': item['songName'],
+                    'artists': item['singerName'],
+                    'type': 'custom',
+                    'subtype': 'migu'
+                }
+            )
+        return song_list
+    except KeyError as e:
+        logger.warning(f'No Results: {e}')
+        return None
